860093b46c28e76c1dfdff877a7c40c9c0cd184c
s4nyam <sanyamlala@gmail.com>
2024-02-12 13:35:40 +0100

    fixed

---
+++ README.md
@@ -0,0 +1,248 @@
+
+<div align="center">
+<img src="https://dl3.pushbulletusercontent.com/qzGTBQatkBVyqaRaJt1rgaLuBPXLmS08/ncalogo-removebg-preview.png" alt="logo" width="50%"></img>
+</div>
+
+# Master's Thesis: AI Generating Algorithms with Self-Organizing Neural Cellular Automata
+<a target="_blank" href="https://colab.research.google.com/drive/1HYKttER_0I6HD1y1oDdg_MLG0D21vMxB?usp=sharing">
+  <img src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open In Colab"/>
+</a> <a target="_blank" href="https://www.python.org/dev/peps/pep-0008/">
+  <img src="https://img.shields.io/badge/code%20style-PEP%208-blueviolet.svg" alt="python style"/>
+</a> <a target="_blank" href="https://nbviewer.org/urls/nca-exp10.s3.amazonaws.com/NonUniform_NCA_v1_1_colab_7Dec.ipynb">
+  <img src="https://user-images.githubusercontent.com/2791223/29387450-e5654c72-8294-11e7-95e4-090419520edb.png" alt="python style"/>
+</a> 
+
+
+
+
+## The Project
+<a target="_blank" href="https://www.hiof.no/english/">
+  <img src="https://dl3.pushbulletusercontent.com/IeVNnAuYX4XQODiVTL9Ln3VWATf5azCc/hio.png" alt="HiOF logo" width="200px"/>
+</a><br/>
+This repo is code supplement for MSc. thesis at <a href="https://www.hiof.no/english/">HiØ</a> under supervision of prof <a href="https://www.nichele.eu/">Stefano Nichele</a> for the June, 2024.
+
+This project implements the Neural CA Frameowork where self-replication with mutation is the only way for cells to live longer. We study and Analyse the growht in Phenotypic Diversity (PD) and Genetic Diversity (GD) over longer time steps. Specifically, we intend to study, formalise and propose three tools to analyse GD and four tools for PD. Specifically, GD level tools are (1) Random Weight Selection Plot (RWSP), (2) Clustering Neural Weights Approach (CNWA) and (3) Genotypic Hash Coloring (GHC). PD level tools include (1) Cellular Type Frequency Plot (CTFP), (2) Global Entropy Plot (GEP), (3) Gross Cell Variance Plot (GCVP), and (4) Cell Local Organisation Global Variance (CLOGV).
+
+Research Questions:
+1. What are the key species that emerge, survive, reproduce and becomes extinct (dead)
+in the evolved NCA model?
+2. How does the genotypic diversity evolve over successive generations of the NCA
+model?
+3. What is the impact of the self-replication and self-maintenance processes, characteristic
+of autopoiesis, on the genetic makeup of the evolving NCAs?
+4. How do phenotypic variations manifest within the NCA model, and what are the
+underlying factors contributing to this diversity?
+5. To what extent do the cellular activities within the NCA exhibit sensitivity to initial
+conditions (for example number of time steps) and how does this influence the genetic
+and phenotypic outcomes?
+
+Objectives:
+1. Analyze the species composition within the evolved NCA model using Cellular Type
+Frequency Plot and identify dominant and rare species.
+2. Implement Genotypic Hash Coloring and Random Weight Selection Plot to quantify
+and visualize changes in genotypic diversity across multiple generations of NCAs.
+3. Apply Clustering Neural Weights Approach to understand how self-replication and
+autopoiesis contribute to the organization and maintenance of genetic traits within
+the NCAs.
+4. Utilize Phenotypic Diversity tools, including Global Entropy Plot and Gross Cell
+Variance Plot, to assess and quantify the diversity in emergent phenotypes over the
+course of NCA evolution.
+5. Investigate the impact of dynamic landscapes on the evolution of smart and interesting
+phenomena, exploring adaptability and evolvability within the NCA model.
+6. Evaluate the effectiveness of the proposed tools in providing fine-grained and coarse-
+grained analysis of Non-Uniform Self-Replicating NCAs.
+7. Assess the potential applications of the research findings in advancing the understanding
+of life, biology, and complex systems, as well as their implications for artificial general
+intelligence.
+
+Please refer to report document for further conceptual and implementation details.
+
+### Contents
+* [Open in Colab](#open-in-colab)
+* [Using SLURM for Larger Experiments](#using-slurm-for-larger-experiments)
+* [Configuration on Simula eX3](#configuration-on-simula-ex3)
+* [SageMaker instance](#sagemaker-instance)
+* [Cite this](#cite-this)
+* [Sample 900 steps](#sample-900-steps)
+
+
+## Open in Colab
+<a target="_blank" href="https://colab.research.google.com/drive/1HYKttER_0I6HD1y1oDdg_MLG0D21vMxB?usp=sharing">
+  <img src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open In Colab"/>
+</a>
+
+Use the colab button above to directly start reproducing the code. You can also import [1_colab.ipynb](https://github.com/s4nyam/MasterThesis/blob/main/1_colab.ipynb) into your notebook / ipython environment. The advantage of Google Colab is to debug and quickly test the actual framework. We set the following parameters in first cell of the notebook and rest of the cooking material evolves the NCA and generate corresponding results.
+
+```python
+import torch
+precision = 1
+torch.set_printoptions(precision=precision)
+WIDTH, HEIGHT = 30,30
+grid_size = (WIDTH, HEIGHT)
+print("Width and Height used are {} and {}".format(WIDTH, HEIGHT))
+INIT_PROBABILITY = 0.02
+min_pixels = max(0, int(WIDTH * HEIGHT * INIT_PROBABILITY))
+NUM_LAYERS = 2  # One hidden and one alpha
+ALPHA = 0.6  # To make other cells active
+INHERTIANCE_PROBABILITY = 0.2  # probability that neighboring cells will inherit by perturbation.
+parameter_perturbation_probability = 0.2
+print("Numbers of layers used are {}".format(NUM_LAYERS))
+print("1 for alpha layer and rest {} for hidden".format(NUM_LAYERS-1))
+NUM_STEPS = 90
+num_steps = NUM_STEPS
+print("Numbers of Time Steps are {}".format(NUM_STEPS))
+DEVICE = torch.device("cuda"  if torch.cuda.is_available() else  "cpu")
+print(f"Using device: {DEVICE}")
+activation = 'sigmoid'  # ['relu','sigmoid','tanh','leakyrelu']
+frequency_dicts = []
+FPS = 2  # Speed of display for animation of NCA and plots
+marker_size = 2  # for plots
+everystep_weights = [] # Stores weigths of the NNs from every time step.
+```
+With this configuration it simulates the proposed NCA framework. And then results a simulation like:
+![sim](https://github.com/s4nyam/MasterThesis/assets/13884479/6854c5f4-dc25-404d-874a-28e97f363df0)
+
+
+Beyond that it produces more results that are related to Phenotypic Diversity (PD) and Genotypic Diversity. PD tools are shown below:
+<div align="center">
+<img src="https://dl3.pushbulletusercontent.com/7bI309gDF8A9kVN8bv73gQKMwkRo22io/pdtools.gif" width=100% alt="logo"></img>
+</div>
+
+And GD tools for example 
+* Genotypic Hash Coloring
+<div align="center">
+<img src="https://dl3.pushbulletusercontent.com/GRf9FgtTCiyHPbIvemcVvS9qgDNpAItq/download.gif" width=50% alt="logo"></img>
+</div>
+
+* Speciation Plot using Clustering Neural Weights Approach:
+<div align="center">
+<img src="https://dl3.pushbulletusercontent.com/0HSjDgttZncmwsen0wyc186r7h3fYBUs/download.png" width=50% alt="logo"></img>
+</div>
+
+* Random Weight Selection Plot 
+<div align="center">
+<img src="https://dl3.pushbulletusercontent.com/VIXuT827El7mWMtapTpfZh5Nahqaw5LN/rwsp.gif" width=50% alt="logo"></img>
+</div>
+
+## Using SLURM for Larger Experiments
+We use Simula eX3 cluster in support of Research Council Norway. We use a100q, dgx2q and hgx2q machines to run our larger experiments. The batch script looks like:
+```bash
+#!/bin/bash
+#SBATCH -p a100q ## hgx2q, a100q,  dgx2q (old)
+#SBATCH --output=run.out    ## Output file
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --gres=gpu:1
+#SBATCH -t 13-00:00 # time (D-HH:MM)
+#SBATCH --mail-user=XXXXX
+## module load cuda11.0/toolkit/11.0.3 
+## module load cudnn8.0-cuda11.0
+#module load cuda11.3/toolkit/11.3.0 
+#module load cudnn8.0-cuda11.3
+module use  /cm/shared/ex3-modules/0.4.1/modulefiles
+##module load slurm/20.02.7
+##module load python-3.9.15
+# ACTIVATE ANACONDA
+eval "$(conda shell.bash hook)"
+conda activate new_env
+srun python -u run.py 
+```
+However the run.py file contains similar code that we have seen in Google Colab, but only produces large archives for evolved weights and the grid, which can further be analysed using the SageMaker notebook. For instance, the ```run.py``` file has following ingredient:
+```python
+precision = 1
+WIDTH, HEIGHT = 100,100
+grid_size = (WIDTH, HEIGHT)
+INIT_PROBABILITY = 0.0030
+min_pixels = max(0, int(WIDTH * HEIGHT * INIT_PROBABILITY))
+NUM_LAYERS = 2 # One hidden and one alpha
+ALPHA = 0.6 # To make other cells active
+INHERTIANCE_PROBABILITY  = 0.2 # probability that neighboring cells will inherit by perturbation.
+parameter_perturbation_probability = 0.2
+NUM_STEPS = 1000, 2000, 3000, ..., 10000
+num_steps = NUM_STEPS
+activation = 'sigmoid' # ['relu','sigmoid','tanh','leakyrelu']
+FPS = 1 # Speed of display for animation of NCA and plots
+marker_size = 2 # for plots
+frequency_dicts = []
+everystep_weights = [] 
+ca_grids_for_later_analysis_layer0 = []
+ca_grids_for_later_analysis_layer1 = []
+...
+# Evolve NCA weights and grid
+...
+with open('frequency_dicts.pkl', 'wb') as f1, open('everystep_weights.pkl', 'wb') as f2, open('ca_grids_layer0.pkl', 'wb') as f3, open('ca_grids_layer1.pkl', 'wb') as f4:
+        pickle.dump(frequency_dicts, f1)
+        pickle.dump(everystep_weights, f2)
+        pickle.dump(ca_grids_for_later_analysis_layer0, f3)
+        pickle.dump(ca_grids_for_later_analysis_layer1, f4)
+```
+We release all required code in the ```main runs``` folder. The requirements to start with the SLURM code is as follows. 
++ Create environment and install using ```requirements.txt```
+```
+conda create --name myenv 
+conda activate myenv 
+conda install --file requirements.txt
+```
++ ```requirements.txt```
+```
+# NumPy for numerical operations 
+numpy==1.21.0 
+# Matplotlib for plotting 
+matplotlib==3.4.3 
+# PyTorch for machine learning 
+torch==1.10.0 
+# Pandas for data manipulation and analysis 
+pandas==1.3.3
+```
+## Configuration on Simula eX3
+Following configuration was used with multiple nodes of each platform to fasten the multi-fold experiments. 
+| Platform  | Model   | GPU            | Processor                                      | 
+|-----------|---------|----------------|------------------------------------------------|
+| x64_64/A100 | hgx2q | g002 | DualProcessor AMD EPYC Milan 7763 64-core w/ 8 qty Nvidia Volta A100/80GB |
+| x64_64/V100 | dgx2q | g001 | DualProcessor Intel Xeon Scalable Platinum 8176 w/ 16 qty Nvidia Volta V100 |
+
+## SageMaker instance
+We use AWS SageMaker for the easiness to process experimental results. Specifically, for the exports that we receive from SLURM experiments, which are as pickle files containing evolved weights and stored grids. SageMaker notebook instance used with almost 500 GB of storage and following system configurations as per the size of experiment exports.
+| Instance Type    | vCPU | Memory  | Price per Hour | Experiment size |
+|------------------|------|---------|-----------------|-------------|
+| ml.r5.large      | 2    | 16 GiB  | $0.151         |<500 MB|
+| ml.r5.xlarge     | 4    | 32 GiB  | $0.302         |<1 GB|
+| ml.r5.2xlarge    | 8    | 64 GiB  | $0.605         |<2 GB|
+| ml.r5.4xlarge    | 16   | 128 GiB | $1.21          |<10 GB|
+| ml.r5.8xlarge    | 32   | 256 GiB | $2.419         |<15 GB|
+| ml.r5.12xlarge   | 48   | 384 GiB | $3.629         |<20 GB|
+| ml.r5.16xlarge   | 64   | 512 GiB | $4.838         |<30 GB|
+| ml.r5.24xlarge   | 96   | 768 GiB | $7.258         |<100 GB|
+
+This classification of Experiment Size is done to avoid runtime collapse because of the memory outage during the processing of the results for the experiments. We could have produced results on runtime while experimenting using SLURM experiments, however to modularise and balance the workload, we preferred to save the weights for one time, and then process results later if required. This also brought flexibility to improve existing code bases for processing results and add more tools later for our project. We release ```2_sagemaker.ipynb``` to process the experiments. This notebook takes public URLs of the experimentation and process results. We also release public URLs of the experiments that can be processed using this notebook.
+
+| Experiment | Download Link                                      |
+|------------|-----------------------------------------------------|
+| exp1s      | [Download exp1s.tar](https://nca-exp10.s3.amazonaws.com/exp1s.tar)  |
+| exp2s      | [Download exp2s.tar](https://nca-exp10.s3.amazonaws.com/exp2s.tar)  |
+| exp3s      | [Download exp3s.tar](https://nca-exp10.s3.amazonaws.com/exp3s.tar)  |
+| exp4s      | [Download exp4s.tar](https://nca-exp10.s3.amazonaws.com/exp4s.tar)  |
+| exp5s      | [Download exp5s.tar](https://nca-exp10.s3.amazonaws.com/exp5s.tar)  |
+| exp6s      | [Download exp6s.tar](https://nca-exp10.s3.amazonaws.com/exp6s.tar)  |
+
+Further we release results for all these raw experiments here at this [Drive Link](https://bit.ly/smallruns)
+
+## Cite this
+To cite this repository:
+
+```
+@software{aiganca2023github,
+  author = {Sanyam Jain and Stefano Nichele},
+  title = {{AIGA}: Self-Replicating NCA with Coarse Grained Analysis},
+  url = {http://github.com/s4nyam/MasterThesis},
+  version = {1.0.1},
+  year = {2023},
+}
+```
+
+
+## Sample 900 steps
+
+https://github.com/s4nyam/MasterThesis/assets/13884479/57d5e84b-9381-445e-b9d3-342f065d89b7
+
+
---
+++ old_clusters/runs_simula/20by20_number_layers=3/batch1/exp14/run.py
@@ -0,0 +1,342 @@
+precision = 1
+WIDTH, HEIGHT = 20,20
+grid_size = (WIDTH, HEIGHT)
+INIT_PROBABILITY = 0.05
+min_pixels = max(0, int(WIDTH * HEIGHT * INIT_PROBABILITY))
+NUM_LAYERS = 3 # One hidden and one alpha
+ALPHA = 0.6 # To make other cells active
+INHERTIANCE_PROBABILITY  = 0.1 # probability that neighboring cells will inherit by perturbation.
+parameter_perturbation_probability = 0.05
+NUM_STEPS = 1000
+num_steps = NUM_STEPS
+activation = 'sigmoid' # ['relu','sigmoid','tanh','leakyrelu']
+FPS = 1 # Speed of display for animation of NCA and plots
+marker_size = 2 # for plots
+
+
+frequency_dicts = []
+everystep_weights = [] 
+ca_grids_for_later_analysis_layer0 = []
+ca_grids_for_later_analysis_layer1 = []
+ca_grids_for_later_analysis_layer2 = []
+
+import pickle
+import torch
+torch.set_printoptions(precision=precision)
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+import pickle
+import torch
+import torch.nn as nn
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import FFMpegWriter
+import random
+import copy
+from collections import Counter
+import os
+frame_folder = 'frames'
+os.makedirs(frame_folder, exist_ok=True)
+# Define a function to save a frame with a formatted file name
+def save_frame(ca_grid, frame_number):
+    for layer in range(NUM_LAYERS):
+        plt.imshow(ca_grid[layer].cpu().numpy(), cmap=colormaps[layer])
+        plt.title(f'Layer {layer + 1}')
+        fig = plt.gcf()
+        fig.set_dpi(800)
+        plt.savefig(os.path.join(frame_folder, f'frame_{frame_number:07d}_layer_{layer}.pdf'), format='pdf', dpi=fig.get_dpi())
+        plt.close()
+
+
+ca_grids_for_later_analysis = []
+
+# def custom_activation(x):
+#     result = 0.05 * (x + 2)
+#     result = max(0, min(0.1, result))
+#     return result
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+if(activation == 'sigmoid'):
+  class SimpleNN(nn.Module):
+      def __init__(self):
+          super(SimpleNN, self).__init__()
+          self.fc1 = nn.Linear(9 * NUM_LAYERS, NUM_LAYERS)
+          self.sigmoid = nn.Sigmoid()
+          self.fc2 = nn.Linear(NUM_LAYERS, NUM_LAYERS)
+          # Initialize weights and biases to zero
+          nn.init.zeros_(self.fc1.weight)
+          nn.init.zeros_(self.fc2.weight)
+          nn.init.zeros_(self.fc1.bias)
+          nn.init.zeros_(self.fc2.bias)
+      def forward(self, x):
+          x = self.fc1(x)
+          x = self.sigmoid(x)
+          x = self.fc2(x)
+          return x
+
+elif(activation == 'tanh'):
+  class SimpleNN(nn.Module):
+      def __init__(self):
+          super(SimpleNN, self).__init__()
+          self.fc1 = nn.Linear(9 * NUM_LAYERS, NUM_LAYERS)
+          self.tanh = nn.Tanh()
+          self.fc2 = nn.Linear(NUM_LAYERS, NUM_LAYERS)
+          # Initialize weights and biases to zero
+          nn.init.zeros_(self.fc1.weight)
+          nn.init.zeros_(self.fc2.weight)
+          nn.init.zeros_(self.fc1.bias)
+          nn.init.zeros_(self.fc2.bias)
+      def forward(self, x):
+          x = self.fc1(x)
+          x = self.tanh(x)
+          x = self.fc2(x)
+          return x
+
+
+elif(activation == 'relu'):
+  class SimpleNN(nn.Module):
+      def __init__(self):
+          super(SimpleNN, self).__init__()
+          self.fc1 = nn.Linear(9 * NUM_LAYERS, NUM_LAYERS)
+          self.relu = nn.ReLU()
+          self.fc2 = nn.Linear(NUM_LAYERS, NUM_LAYERS)
+          # Initialize weights and biases to zero
+          nn.init.zeros_(self.fc1.weight)
+          nn.init.zeros_(self.fc2.weight)
+          nn.init.zeros_(self.fc1.bias)
+          nn.init.zeros_(self.fc2.bias)
+      def forward(self, x):
+          x = self.fc1(x)
+          x = self.relu(x)
+          x = self.fc2(x)
+          return x
+else:
+  class SimpleNN(nn.Module):
+      def __init__(self):
+          super(SimpleNN, self).__init__()
+          self.fc1 = nn.Linear(9 * NUM_LAYERS, NUM_LAYERS)
+          self.leaky_relu = nn.LeakyReLU()
+          self.fc2 = nn.Linear(NUM_LAYERS, NUM_LAYERS)
+          # Initialize weights and biases to zero
+          nn.init.zeros_(self.fc1.weight)
+          nn.init.zeros_(self.fc2.weight)
+          nn.init.zeros_(self.fc1.bias)
+          nn.init.zeros_(self.fc2.bias)
+      def forward(self, x):
+          x = self.fc1(x)
+          x = self.leaky_relu(x)
+          x = self.fc2(x)
+          return x
+
+
+ca_nn = SimpleNN().to(DEVICE)
+
+# Calculate the number of parameters stepwise
+
+input_size_fc1 = 9 * NUM_LAYERS
+output_size_fc1 = 2
+weight_params_fc1 = input_size_fc1 * output_size_fc1
+bias_params_fc1 = output_size_fc1
+
+input_size_fc2 = 2
+output_size_fc2 = NUM_LAYERS
+weight_params_fc2 = input_size_fc2 * output_size_fc2
+bias_params_fc2 = output_size_fc2
+
+weight_params = weight_params_fc1 + weight_params_fc2
+bias_params = bias_params_fc1 + bias_params_fc2
+total_params = weight_params + bias_params
+
+
+
+ca_grid = torch.zeros((NUM_LAYERS, WIDTH, HEIGHT), device=DEVICE, dtype=torch.float32)
+random_tensor = torch.ones((WIDTH, HEIGHT))
+sorted_values, indices = random_tensor.view(-1).sort()
+mask = torch.zeros_like(random_tensor)
+shuffled_indices = indices.tolist()  # Convert indices to a list
+random.shuffle(shuffled_indices)     # Shuffle the list
+random_positions = shuffled_indices[:min_pixels]
+mask.view(-1)[random_positions] = 1.0
+for layer in range(0,NUM_LAYERS):
+  if (layer==0):
+    ca_grid[layer] = mask * (ALPHA + (1 - ALPHA) * random_tensor)  # Initialize the alpha channel with values greater than ALPHA
+  else:
+    ca_grid[layer] = mask * (ALPHA + (1 - ALPHA) * random_tensor * random.random())  # Initialize the other channel with values greater than ALPHA
+
+def initialize_weights(module):
+    if isinstance(module, nn.Linear):
+        nn.init.xavier_normal_(module.weight)
+        nn.init.zeros_(module.bias)
+def initialize_weights_to_zero(module):
+    if isinstance(module, nn.Linear):
+        nn.init.zeros_(module.weight)
+        nn.init.zeros_(module.bias)
+
+# Create a list of neural networks, one for each pixel
+ca_nn_list = [SimpleNN().to(DEVICE) for _ in range(WIDTH * HEIGHT)]
+
+# Initialize the weights for neural networks associated with live pixels
+for i in range(WIDTH):
+    for j in range(HEIGHT):
+        if ca_grid[0, i, j] > ALPHA:
+            ca_nn_list[i * WIDTH + j].apply(initialize_weights)
+
+
+def update_ca(ca_grid, ca_nn):
+    new_ca_grid = ca_grid.clone()
+    def process_neighborhood(i, j, idx):
+        neighborhood = torch.zeros(9 * NUM_LAYERS, device=DEVICE)
+        for dx in [-1, 0, 1]:
+            for dy in [-1, 0, 1]:
+                ni, nj = (i + dx) % WIDTH, (j + dy) % HEIGHT
+                for l in range(NUM_LAYERS):
+                    neighborhood[(dx + 1) * 3 + (dy + 1) + l * 9] = ca_grid[l, ni, nj]
+        neighborhood = torch.unsqueeze(neighborhood, 0)
+        output = None  # Initialize output to None
+
+        if((neighborhood > ALPHA).any()):
+          output = ca_nn_list[idx](neighborhood)
+          if(random.random() < INHERTIANCE_PROBABILITY):
+            high_alpha_pixels = [(dx, dy) for dx in [-1, 0, 1] for dy in [-1, 0, 1] if ca_grid[0, (i + dx) % WIDTH, (j + dy) % HEIGHT] > ALPHA] # Find neighboring pixels with alpha values greater than ALPHA
+            if high_alpha_pixels:
+                # check again for livelihood of the pixels (if this if condition passed, that means there is at least one element that is greater than alpha.)
+                current_alpha_value = ca_grid[0, i, j]
+                high_alpha_values = [ca_grid[0, (i + dx) % WIDTH, (j + dy) % HEIGHT] for dx, dy in high_alpha_pixels]
+                selected_pixel = random.choice(high_alpha_pixels)  # Select any random live pixel in the neighborhood
+                ni, nj = (i + selected_pixel[0]) % WIDTH, (j + selected_pixel[1]) % HEIGHT
+
+                # Check if the selected neural network has any non-zero weights
+                selected_nn_idx = ni * WIDTH + nj
+                selected_nn = ca_nn_list[selected_nn_idx]
+                has_nonzero_weights = any(torch.any(param != 0) for param in selected_nn.parameters())
+
+                if not has_nonzero_weights:
+                    # Copy the neural network from the selected neighboring pixel
+                    ca_nn_list[idx] = copy.deepcopy(selected_nn)
+
+                    # Perturb neural network weights
+                    for name, param in ca_nn_list[idx].named_parameters():
+                        if 'weight' in name:
+                            if random.random() < parameter_perturbation_probability:
+                                param.data += torch.randn_like(param.data) * random.uniform(-1, 1)
+                else:
+                    ca_nn_list[idx] = copy.deepcopy(selected_nn)
+        else:
+          ca_nn_list[idx].apply(initialize_weights_to_zero)
+
+
+        if output is not None:
+            return output.squeeze().tolist()
+        else:
+            return [0.0] * NUM_LAYERS
+
+    idx = 0  # Index for the neural networks
+    for i in range(WIDTH):
+        for j in range(HEIGHT):
+            updated_values = process_neighborhood(i, j, idx)
+            for layer in range(NUM_LAYERS):
+                new_ca_grid[layer, i, j] = updated_values[layer]
+            idx += 1
+
+    new_ca_grid_temp = new_ca_grid.clone()
+    # now replace all pixels with their corresponding sigmoid values.
+    for x in range(WIDTH):
+      for y in range(HEIGHT):
+        for layer in range(NUM_LAYERS):
+          updated_value = sigmoid(new_ca_grid_temp[layer, x, y].cpu().numpy())
+          new_ca_grid_temp[layer, x, y] = updated_value # setting a value between 0 and 1 for the alive pixels.
+
+    # now we will check for the ALPHA values as threshold
+    for x in range(WIDTH):
+      for y in range(HEIGHT):
+          # Check if any value in channel 0 is less than ALPHA at the current position
+          if (new_ca_grid_temp[0, x, y] < ALPHA):
+            # If any value is less than ALPHA, set values in all layers at the current position to 0
+            for layer in range(NUM_LAYERS):
+                new_ca_grid_temp[layer, x, y] = 0.0
+
+    return new_ca_grid_temp
+
+
+
+
+metadata = dict(title='Neural CA Simulation', artist='AI', comment='Neural CA Simulation')
+writer = FFMpegWriter(fps=FPS, metadata=metadata)
+all_colormaps = plt.colormaps()
+colormaps = all_colormaps
+fig, axes = plt.subplots(1, NUM_LAYERS, figsize=(5 * NUM_LAYERS, 5))
+plt.tight_layout()
+plt.close(fig)
+import time
+stamp = int(time.time())
+
+for frame in range(NUM_STEPS+1):
+    # append NN weithts here
+    weights_list = []
+    for network in ca_nn_list:
+        state_dict = network.state_dict()
+        flattened_params = []
+        for param in state_dict.values():
+            flattened_params.extend(param.view(-1).tolist())
+        weights_list.append(flattened_params)
+    everystep_weights.append(weights_list)
+    if(frame == 0):
+      ca_grid = ca_grid
+      for layer in range(NUM_LAYERS):
+          ax = axes[layer]
+          ax.clear()
+          ax.imshow(ca_grid[layer].cpu().numpy(), cmap=colormaps[0])
+          ax.set_title(f'Layer {layer + 1}')
+      save_frame(ca_grid, frame)
+    else:
+      ca_grid = update_ca(ca_grid, ca_nn)
+      ca_grids_for_later_analysis_layer0.append(ca_grid[0].cpu().numpy())
+      ca_grids_for_later_analysis_layer1.append(ca_grid[1].cpu().numpy())
+      ca_grids_for_later_analysis_layer2.append(ca_grid[2].cpu().numpy())
+      precision_multiplier = 10 ** precision
+      rounded_grid = (ca_grid[0] * precision_multiplier).round() / precision_multiplier # picking only ALPHA values for plot!!!!!!
+      unique_values, value_counts = torch.unique(rounded_grid, return_counts=True)
+      frequency_dict = {value.item(): count.item() for value, count in zip(unique_values, value_counts)}
+      frequency_dicts.append(frequency_dict)
+      for layer in range(NUM_LAYERS):
+          ax = axes[layer]
+          ax.clear()
+          ax.imshow(ca_grid[layer].cpu().numpy(), cmap=colormaps[0])
+          ax.set_title(f'Layer {layer + 1}')
+      save_frame(ca_grid, frame)
+    if(frame%20==0 or frame<20):
+      with open('frequency_dicts.pkl', 'wb') as f1, open('everystep_weights.pkl', 'wb') as f2, open('ca_grids_layer0.pkl', 'wb') as f3, open('ca_grids_layer1.pkl', 'wb') as f4, open('ca_grids_layer2.pkl', 'wb') as f5:
+        pickle.dump(frequency_dicts, f1)
+        pickle.dump(everystep_weights, f2)
+        pickle.dump(ca_grids_for_later_analysis_layer0, f3)
+        pickle.dump(ca_grids_for_later_analysis_layer1, f4)
+        pickle.dump(ca_grids_for_later_analysis_layer2, f5)
+
+
+
+# Save these three lists to storage and know method to reload them
+# frequency_dicts = []
+# everystep_weights = [] 
+# ca_grids_for_later_analysis_layer0 = []
+# ca_grids_for_later_analysis_layer1 = []
+# ca_grids_for_later_analysis_layer2 = []        
+
+
+# #Reload
+# import pickle
+
+# # Load the lists from the binary files
+# with open('frequency_dicts.pkl', 'rb') as f1, open('everystep_weights.pkl', 'rb') as f2, open('ca_grids_layer0.pkl', 'rb') as f3, open('ca_grids_layer1.pkl', 'rb') as f4, open('ca_grids_layer2.pkl', 'rb') as f5:
+#     frequency_dicts = pickle.load(f1)
+#     everystep_weights = pickle.load(f2)
+#     ca_grids_for_later_analysis_layer0 = pickle.load(f3)
+#     ca_grids_for_later_analysis_layer1 = pickle.load(f4)
+#     ca_grids_for_later_analysis_layer2 = pickle.load(f5)
+
+
+# # Now, you have your lists back in memory
+# print(frequency_dicts)
+# print(everystep_weights)
+# print(ca_grids_for_later_analysis_layer0)
+# print(ca_grids_for_later_analysis_layer1)
+# print(ca_grids_for_later_analysis_layer2)
